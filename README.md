e5fd9beae88a5cf70115cad912e861fbeb8185f0
Shadow591 <Mickey908@gmail.com>
2018-11-23 20:13:23 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# deluge-chef-config
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: deluge-chef-config
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
