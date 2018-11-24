4948c102ad273cd4d9616ef72a8a1e227599362f
Shadow591 <Mickey908@gmail.com>
2018-11-24 09:42:59 -0600

    deluge install

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,38 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_repository 'deluge' do
+  uri 'ppa:deluge-team/ppa'
+end
+
+package 'deluge package' do
+  package_name %w(deluged deluge-webui)
+end
+
+user 'deluge' do
+  system true
+end
+
+group 'deluge' do
+  members 'deluge'
+  system true
+end
+
+template 'deluged.service' do
+  source 'deluged.service.erb'
+  path '/etc/systemd/system/deluged.service'
+end
+
+service 'deluged' do
+  action [ :enable, :start ]
+end
+
+template 'deluge-web.service' do
+  source 'deluge-web.service.erb'
+  path '/etc/systemd/system/deluge-web.service'
+end
+
+service 'deluge-web' do
+  action [ :enable, :start ]
+end
